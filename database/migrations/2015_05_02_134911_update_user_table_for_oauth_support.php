748e8548310a704527a274a64f3c7ef570733d45
Arif Islam <arif@dreamfactory.com>
2015-05-02 17:48:06 -0400

    updates

--- database/migrations/2015_05_02_134911_update_user_table_for_oauth_support.php
+++ database/migrations/2015_05_02_134911_update_user_table_for_oauth_support.php
@@ -3,40 +3,59 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateUserTableForOauthSupport extends Migration {
+class UpdateUserTableForOauthSupport extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		if(!Schema::hasColumn('user', 'oauth_provider'))
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if ( !Schema::hasColumn( 'user', 'oauth_provider' ) )
         {
-            Schema::table('user', function(Blueprint $t)
+            Schema::table(
+                'user',
+                function ( Blueprint $t )
                 {
-                    $t->string('oauth_provider', 50)->nullable()->after('remember_token');
+                    $t->string( 'oauth_provider', 50 )->nullable()->after( 'remember_token' );
                 }
             );
         }
-	}
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-        if(Schema::hasColumn('user', 'oauth_provider'))
+        Schema::create(
+            'oauth_config',
+            function ( Blueprint $t )
+            {
+                $t->integer( 'service_id' )->unsigned()->primary();
+                $t->foreign( 'service_id' )->references( 'id' )->on( 'service' )->onDelete( 'cascade' );
+                $t->string( 'provider' );
+                $t->string( 'client_id' );
+                $t->longText( 'client_secret' );
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if ( Schema::hasColumn( 'user', 'oauth_provider' ) )
         {
-            Schema::table('user', function(Blueprint $t)
+            Schema::table(
+                'user',
+                function ( Blueprint $t )
                 {
-                    $t->dropColumn('oauth_provider');
+                    $t->dropColumn( 'oauth_provider' );
                 }
             );
         }
-	}
+
+        Schema::dropIfExists('oauth_config');
+    }
 
 }
---
+++ src/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the DreamFactory Rave(tm)
+ *
+ * DreamFactory Rave(tm) <http://github.com/dreamfactorysoftware/rave>
+ * Copyright 2012-2014 DreamFactory Software, Inc. <support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace DreamFactory\DSP\OAuth\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        if ( !ServiceType::whereName( 'oauth_facebook' )->exists() )
+        {
+            // Add the service type
+            ServiceType::create(
+                [
+                    'name'           => 'oauth_facebook',
+                    'class_name'     => "DreamFactory\\DSP\\OAuth\\Services\\Facebook",
+                    'config_handler' => "DreamFactory\\DSP\\OAuth\\Models\\OAuthConfig",
+                    'label'          => 'Facebook OAuth',
+                    'description'    => 'OAuth service for supporting Facebook authentication and API access.',
+                    'group'          => 'oauth',
+                    'singleton'      => 1
+                ]
+            );
+            $this->command->info( 'Facebook OAuth service type seeded!' );
+        }
+
+        if ( !ServiceType::whereName( 'oauth_twitter' )->exists() )
+        {
+            // Add the service type
+            ServiceType::create(
+                [
+                    'name'           => 'oauth_facebook',
+                    'class_name'     => "DreamFactory\\DSP\\OAuth\\Services\\Twitter",
+                    'config_handler' => "DreamFactory\\DSP\\OAuth\\Models\\OAuthConfig",
+                    'label'          => 'Twitter OAuth',
+                    'description'    => 'OAuth service for supporting Twitter authentication and API access.',
+                    'group'          => 'oauth',
+                    'singleton'      => 1
+                ]
+            );
+            $this->command->info( 'Twitter OAuth service type seeded!' );
+        }
+
+        if ( !ServiceType::whereName( 'oauth_github' )->exists() )
+        {
+            // Add the service type
+            ServiceType::create(
+                [
+                    'name'           => 'oauth_github',
+                    'class_name'     => "DreamFactory\\DSP\\OAuth\\Services\\Github",
+                    'config_handler' => "DreamFactory\\DSP\\OAuth\\Models\\OAuthConfig",
+                    'label'          => 'Github OAuth',
+                    'description'    => 'OAuth service for supporting Github authentication and API access.',
+                    'group'          => 'oauth',
+                    'singleton'      => 1
+                ]
+            );
+            $this->command->info( 'Github OAuth service type seeded!' );
+        }
+    }
+}
