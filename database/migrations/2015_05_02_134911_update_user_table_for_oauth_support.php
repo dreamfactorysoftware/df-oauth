51edd8f999129333869a97363d91d50ff1b25f27
Arif Islam <arif@dreamfactory.com>
2015-11-13 16:44:24 -0500

    reverting changes made for SQL Server compatibility

--- database/migrations/2015_05_02_134911_update_user_table_for_oauth_support.php
+++ database/migrations/2015_05_02_134911_update_user_table_for_oauth_support.php
@@ -28,7 +28,7 @@ class UpdateUserTableForOauthSupport extends Migration
                 $t->integer('service_id')->unsigned()->primary();
                 $t->foreign('service_id')->references('id')->on('service')->onDelete('cascade');
                 $t->integer('default_role')->unsigned()->index();
-                $t->foreign('default_role')->references('id')->on('role')->onDelete('no action');
+                $t->foreign('default_role')->references('id')->on('role')->onDelete('restrict');
                 $t->string('client_id');
                 $t->longText('client_secret');
                 $t->string('redirect_url');
