e498eafc1c932761500451a66dc861ab5c143808
Lee Hicks <leehicks@dreamfactory.com>
2017-11-02 11:56:19 -0400

    cleanup openapi output

--- src/Resources/SSO.php
+++ src/Resources/SSO.php
@@ -38,41 +38,44 @@ class SSO extends BaseRestResource
         $path = '/' . $resourceName;
         $service = $this->getServiceName();
         $capitalized = camelize($service);
-        $class = trim(strrchr(static::class, '\\'), '\\');
 
         $base = [
             $path => [
                 'post' => [
-                    'summary'     => 'perform' . $capitalized . $class . 'SSO() - Single Sign On',
-                    'operationId' => 'perform' . $capitalized . $class . 'SSO',
+                    'summary'     => 'perform' . $capitalized . 'SSO() - Single Sign On',
+                    'operationId' => 'perform' . $capitalized . 'SSO',
                     'description' => 'Performs Single Sign On using OAuth 2.0 access token',
                     'requestBody' => [
                         'description' => 'Content - OAuth token response',
-                        'schema'      => [
-                            'type'       => 'object',
-                            'required'   => ['access_token', 'token_type'],
-                            'properties' => [
-                                'access_token'  => [
-                                    'type'        => 'string',
-                                    'description' => 'The access token issued by the authorization server.'
+                        'content'     => [
+                            'application/json' => [
+                                'schema' => [
+                                    'type'       => 'object',
+                                    'required'   => ['access_token', 'token_type'],
+                                    'properties' => [
+                                        'access_token'  => [
+                                            'type'        => 'string',
+                                            'description' => 'The access token issued by the authorization server.'
+                                        ],
+                                        'token_type'    => [
+                                            'type'        => 'string',
+                                            'description' => 'The type of the token. Typically Bearer.'
+                                        ],
+                                        'expires_in'    => [
+                                            'type'        => 'integer',
+                                            'description' => 'The lifetime in seconds of the access token.'
+                                        ],
+                                        'refresh_token' => [
+                                            'type'        => 'string',
+                                            'description' => 'The refresh token, which can be used to obtain new access tokens.'
+                                        ],
+                                        'scope'         => [
+                                            'type'        => 'string',
+                                            'description' => 'OPTIONAL, if identical to the scope requested by the client; otherwise, REQUIRED.'
+                                        ],
+                                    ],
                                 ],
-                                'token_type'    => [
-                                    'type'        => 'string',
-                                    'description' => 'The type of the token. Typically Bearer.'
-                                ],
-                                'expires_in'    => [
-                                    'type'        => 'integer',
-                                    'description' => 'The lifetime in seconds of the access token.'
-                                ],
-                                'refresh_token' => [
-                                    'type'        => 'string',
-                                    'description' => 'The refresh token, which can be used to obtain new access tokens.'
-                                ],
-                                'scope'         => [
-                                    'type'        => 'string',
-                                    'description' => 'OPTIONAL, if identical to the scope requested by the client; otherwise, REQUIRED.'
-                                ],
-                            ]
+                            ],
                         ],
                         'required'    => true
                     ],
