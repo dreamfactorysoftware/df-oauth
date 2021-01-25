dc18c19bc250e268b456a8181c149d08e36e882f
Volodymyr Polishchuk <volodymyrpo@softwareplanet.uk.com>
2021-01-25 14:52:03 +0200

    DFSAAS-159 Build a Heroku add-on
    - Add checking if tables from migration already exists

--- database/migrations/2021_01_11_143453_heroku_addon_sso_tables.php
+++ database/migrations/2021_01_11_143453_heroku_addon_sso_tables.php
@@ -13,26 +13,30 @@ class HerokuAddonSSOTables extends Migration
      */
     public function up()
     {
-        Schema::create(
-            'heroku_addon_sso',
-            function (Blueprint $t){
-                $t->increments('id');
-                $t->integer('service_id')->unsigned()->index();
-                $t->foreign('service_id')->references('id')->on('service')->onDelete('cascade');
-                $t->string('secret');
-                $t->string('secret_type')->default('string');
-            }
-        );
-        Schema::create(
-            'heroku_users_map',
-            function (Blueprint $t){
-                $t->integer('user_id')->index();
-                $t->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
-                $t->uuid('heroku_user_id');
-                $t->dateTime('updated_at');
-                $t->dateTime('created_at');
-            }
-        );
+        if (!Schema::hasTable('heroku_addon_sso')) {
+            Schema::create(
+                'heroku_addon_sso',
+                function (Blueprint $t){
+                    $t->increments('id');
+                    $t->integer('service_id')->unsigned()->index();
+                    $t->foreign('service_id')->references('id')->on('service')->onDelete('cascade');
+                    $t->string('secret');
+                    $t->string('secret_type')->default('string');
+                }
+            );
+        }
+        if (!Schema::hasTable('heroku_users_map')) {
+            Schema::create(
+                'heroku_users_map',
+                function (Blueprint $t){
+                    $t->integer('user_id')->index();
+                    $t->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+                    $t->uuid('heroku_user_id');
+                    $t->dateTime('updated_at');
+                    $t->dateTime('created_at');
+                }
+            );
+        }
     }
 
     /**
