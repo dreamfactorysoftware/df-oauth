702172817df5010c21c0ed7965b2ea83af99c99f
Riley Bolen <rileybolen123@gmail.com>
2024-02-23 08:01:59 -0800

    Revert working changes to existing Provider

--- src/Components/DfOAuthTwoProvider.php
+++ src/Components/DfOAuthTwoProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Arr;
 use Illuminate\Http\RedirectResponse;
 use Laravel\Socialite\Two\InvalidStateException;
 use SocialiteProviders\Manager\OAuth2\User;
-use Log;
 
 /**
  * Trait DfOAuthTwoProvider
@@ -78,36 +77,10 @@ trait DfOAuthTwoProvider
         if ($this->hasInvalidState()) {
             throw new InvalidStateException();
         }
-        if(true){
-            $token_a_response = $this->getAccessTokenResponse($this->getCode());
-            Log::debug("[OAuth] Got response from Token A request: " . json_encode($token_a_response));
-            // exchange OAuth access_token "Token A" for OBO access_token "Token B"
-            $tokenA = $this->parseAccessToken($token_a_response);
-            Log::debug("[OAuth] Token A: " . $tokenA);
-            if(empty($tokenA)) {
-                throw new \InvalidArgumentException('Recieved invalid access_token from initial OAuth /token request.');
-            }else{
-                $graph_response = $this->getGraphTokenResponse($tokenA);
-                Log::debug("[OAuth] Got response from Graph Token B request: " . json_encode($graph_response));
-                $user = $this->createUserFromGraphTokenResponse($graph_response);
 
-                $obo_response = $this->getOBOTokenResponse($tokenA);
-                Log::debug("[OAuth] Got response from Snowflake Token B request: " . json_encode($obo_response));
-                return $this->setUserAccessTokenFromResponse($user, $obo_response);
-            }
-        }
-    }
-    
-    /**
-     * Retrieve user using token response.
-     *
-     * @param $response
-     *
-     * @return $this
-     */
-    public function createUserFromGraphTokenResponse($response)
-    {
-        return $this->mapUserToObject($this->getUserByToken($this->parseAccessToken($response)));
+        $response = $this->getAccessTokenResponse($this->getCode());
+
+        return $this->getUserFromTokenResponse($response);
     }
 
     /**
@@ -117,9 +90,11 @@ trait DfOAuthTwoProvider
      *
      * @return $this
      */
-    public function setUserAccessTokenFromResponse($user, $response)
+    public function getUserFromTokenResponse($response)
     {
-        $token = $this->parseAccessToken($response);
+        $user = $this->mapUserToObject($this->getUserByToken(
+            $token = $this->parseAccessToken($response)
+        ));
 
         $this->credentialsResponseBody = $response;
 
